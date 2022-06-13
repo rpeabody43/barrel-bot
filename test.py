51fa17bb510f33a9e5f242838178634fa968fde5
Ryan Peabody <rpeabody43@gmail.com>
2022-06-13 19:47:09 -0400

    updated test.py

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
+# Strictly for testing the pybaseball / baseballsavant video aspect of the program
 from datetime import date, timedelta
+from lambda_deploy.stats import get_max_barrel as barrel
+from lambda_deploy.video import get_video
 
 def main():
-    # auto_open = True
-    # date = input('YYYY-MM-DD: ')
-    # pitch = barrel(date)
-    # filepath = video_file.get_video(pitch)
+    auto_open = True
+    date = input('YYYY-MM-DD: ')
+    pitch = barrel(date) # Automatically prints link to Baseball Savant page
+    # filepath = get_video(pitch)
     yesterday = date.today() - timedelta(days=1)
     yesterday_str = yesterday.strftime('%Y-%m-%d')
-    print(yesterday_str)
 
 
 if __name__  == '__main__':
